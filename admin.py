9dee8edbe71db790645a51534f621499e86ba2d0
Noah Seger <nseger@texastribune.org>
2011-04-04 12:41:49 -0500

    Added district admin filters and list display columns

--- admin.py
+++ admin.py
@@ -1,4 +1,8 @@
 from django.contrib.gis import admin
 from models import District
 
-admin.site.register(District, admin.GeoModelAdmin)
+class DistrictAdmin(admin.GeoModelAdmin):
+    list_display = ('__unicode__', 'year')
+    list_filter = ('type', 'year')
+
+admin.site.register(District, DistrictAdmin)
