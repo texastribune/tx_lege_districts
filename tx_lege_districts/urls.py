04f50240915ffe6192fc30b36cefecd069408222
Noah Seger <noah@texastribune.org>
2013-01-29 15:24:17 -0600

    Add by_number view to get district shapes by type and number

--- tx_lege_districts/urls.py
+++ tx_lege_districts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('tx_lege_districts.views',
+    url('^by-number/$', 'by_number', name='tx_lege_districts_by_number'),
     url('^lookup/$', 'lookup', name='tx_lege_districts_lookup'),
     url('^map/$', 'map', name='tx_lege_districts_map'),
 )
--- tx_lege_districts/views.py
+++ tx_lege_districts/views.py
@@ -31,6 +31,37 @@ def lookup(request):
     return HttpResponse(json.dumps(data), mimetype='application/json')
 
 
+def by_number(request):
+    """
+    Returns a list of districts for the given type and numbers
+    """
+    district_type = request.GET.get('type')
+    try:
+        numbers = [int(n) for n in request.GET.getlist('number')]
+    except ValueError:
+        numbers = []
+    if not (district_type and numbers):
+        return HttpResponse('bad request', status=402)
+
+    districts = District.objects.filter(type__iexact=district_type)
+    if numbers:
+        districts = districts.filter(number__in=numbers)
+
+    data = []
+    for district in districts:
+        geojson = district.geometry.simplify(0.0001).geojson
+        coordinates = json.loads(geojson)['coordinates']
+        data.append({
+            'name': unicode(district).encode('utf-8'),
+            'type': district.type.lower(),
+            'number': district.number,
+            'coordinates': coordinates,
+            'year': district.year,
+        })
+
+    return HttpResponse(json.dumps(data), mimetype='application/json')
+
+
 def map(request):
     return render_to_response('districts/includes/map.html', {
             'lookup_url': reverse('tx_lege_districts_lookup')
